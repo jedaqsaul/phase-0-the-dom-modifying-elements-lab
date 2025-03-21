818a57fff898438268ccbca44c85dc2dec9b06a2
jedidiah <jedaqsaul@gmail.com>
2025-03-22 00:12:12 +0300

    completed the dom modifying elements lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+document.getElementById("main").remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+newHeader.textContent = "Aquila Jedidiah is the champion";
+
+document.body.appendChild(newHeader);
